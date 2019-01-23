991e8b6d478e922e909d81366143ffefdd6ea283
xxfast <isuru@xfastgames.com>
2019-01-23 17:52:27 +1100

    Initial Commit

---
+++ helloswift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  helloswift
+//
+//  Created by Isuru Rajapakse on 23/1/19.
+//  Copyright © 2019 Isuru Rajapakse. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
